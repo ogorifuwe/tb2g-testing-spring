30878373ca69d4d8098e680a0d59966444721fb1
oi <oifuwe@luc.edu>
2023-08-20 18:52:05 -0500

    intro spring context - junit 5 - inner class config

--- src/test/java/org/springframework/samples/petclinic/oi/junit5/HearingInterpreterInnerClassTest.java
+++ src/test/java/org/springframework/samples/petclinic/oi/junit5/HearingInterpreterInnerClassTest.java
@@ -1,7 +1,35 @@
 package org.springframework.samples.petclinic.oi.junit5;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.samples.petclinic.oi.HearingInterpreter;
+import org.springframework.samples.petclinic.oi.LaurelWordProducer;
+import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
+
 import static org.junit.jupiter.api.Assertions.*;
 
+@SpringJUnitConfig(classes = {HearingInterpreterInnerClassTest.TestConfig.class})
 class HearingInterpreterInnerClassTest {
+
+  @Autowired
+  private HearingInterpreter hearingInterpreter;
+
+  @Test
+  void whatIHeard() {
+    String word = hearingInterpreter.whatIHeard();
+
+    assertEquals("Laurel", word);
+  }
+
+  @Configuration
+  static class TestConfig {
+
+    @Bean
+    HearingInterpreter hearingInterpreter() {
+      return new HearingInterpreter(new LaurelWordProducer());
+    }
+  }
 
 }
