66f92426ce659d70f184afad07578152060bd0ae
oi <oifuwe@luc.edu>
2023-08-20 19:02:17 -0500

    intro spring context - junit 5 - component scan with inner class config

---
+++ src/test/java/org/springframework/samples/petclinic/oi/junit5/HearingInterpreterActiveProfileTest.java
@@ -0,0 +1,29 @@
+package org.springframework.samples.petclinic.oi.junit5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.samples.petclinic.oi.HearingInterpreter;
+import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringJUnitConfig(classes = {HearingInterpreterActiveProfileTest.TestConfig.class})
+public class HearingInterpreterActiveProfileTest {
+
+  @Autowired
+  private HearingInterpreter hearingInterpreter;
+
+  @Test
+  void whatIHeard() {
+    String word = hearingInterpreter.whatIHeard();
+
+    assertEquals("Laurel", word);
+  }
+
+  @Configuration
+  @ComponentScan("org.springframework.samples.petclinic.oi")
+  static class TestConfig {
+  }
+}
